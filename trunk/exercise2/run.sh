75841df1b5f2beefcebfbf322e5526b6fb94d030
mrcotter2013@gmail.com <mrcotter2013@gmail.com@4d6728cc-4268-c6eb-ead0-70304b354550>
2014-09-30 12:09:08 +0000

    Minor changes to comments in script

--- trunk/exercise2/run.sh
+++ trunk/exercise2/run.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
+#
+# Run script by adding a parameter indicating the repeating time, e.g., ./run.sh 10
 # 
 # arg0  folder with TTP files
 # arg1  pattern to identify the TTP problems that should be solved
 # arg2  optimisation approach chosen: 1 - RLS, 2 - 1+1 EA, 3 - Local Search
 # arg3  stopping criterion: number of evaluations without improvement
-# arg4  stopping criterion: time in milliseconds (e.g., 60000 equals 1 minute)
+# arg4  stopping criterion: time in milliseconds (e.g., 60000 equals to 1 minute)
 # 
  
 for i in `seq 1 $1`; do
