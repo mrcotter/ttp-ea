b8cb0c49297073210e8f294d0224abb326be2b6d
mrcotter2013@gmail.com <mrcotter2013@gmail.com@4d6728cc-4268-c6eb-ead0-70304b354550>
2014-10-02 08:17:43 +0000

    Minor changes

--- trunk/exercise3/src/ttp/Node.java
+++ trunk/exercise3/src/ttp/Node.java
@@ -9,15 +9,24 @@ public class Node {
     //The id of the node.
     private final int id;
 
+    //The position of the node.
+    private final double[] position;
+
     //Node constructor
-    public Node(int id) {
+    public Node(int id, double[] position) {
         super();
         this.id = id;
+        this.position = position;
     }
 
     //Return the id of the node
     public int getID() {
         return id;
+    }
+
+    //Return the position of the node
+    public double[] getPosition() {
+        return position;
     }
 
 }
