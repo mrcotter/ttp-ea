5858a1e105c97157eff7c8a118937b73c7456d65
mrcotter2013@gmail.com <mrcotter2013@gmail.com@4d6728cc-4268-c6eb-ead0-70304b354550>
2014-10-02 08:04:28 +0000

    Change crossover from Edge Recombination to PMX

--- exercise3/src/ttp/Optimisation/TSP.java
+++ exercise3/src/ttp/Optimisation/TSP.java
@@ -1,11 +1,6 @@
 package ttp.Optimisation;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Random;
+import java.util.*;
 
 import ttp.Node;
 import ttp.TTPInstance;
@@ -15,63 +10,80 @@ import ttp.TTPSolution;
 public class TSP {
 	
 	private ArrayList<ArrayList<Node>> multiTours;
-	private ArrayList<Node> singleTour = new ArrayList<Node>();
-	private ArrayList<ArrayList<Node>> offsprings = new ArrayList<ArrayList<Node>>();
+	private ArrayList<Node> singleTour;
+    ArrayList<ArrayList<Node>> nextGeneration;
+	private ArrayList<ArrayList<Node>> offsprings;
 
 	private TTPInstance instance;
 	public double Distance_1 = 0.0, Distance_2 = 0.0;
-		
+
+
 	public TSP(TTPInstance instance) {
 		
 		this.instance = instance;
         singleTour = instance.tourList;
-		
-		Distance_1 = totalDistance(singleTour);
-	}
-	
-	public int[] runEA(int popSize, double cross_rate, double mut_rate, int numOfGenerations) {
 
-		ArrayList<Node> temp;
+        offsprings = new ArrayList<ArrayList<Node>>();
+		//Distance_1 = totalDistance(singleTour);
+	}
+
+	
+	public int[] runGA(int popSize, int numOfGenerations, double cross_rate, double mut_rate) {
+
+		ArrayList<Node> temp, best;
+        double tempDistance, bestDistance;
 		generatePopulation(popSize, true);
+
+        multiTours = GA(multiTours, cross_rate, mut_rate);
+        best = findShortest(multiTours);
+        bestDistance = totalDistance(best);
 		
-		for (int i = 0; i < numOfGenerations; i++) {
-            multiTours = EA(multiTours, cross_rate, mut_rate);
+		for (int i = 1; i < numOfGenerations; i++) {
+            multiTours = GA(multiTours, cross_rate, mut_rate);
+            temp = findShortest(multiTours);
+            tempDistance = totalDistance(temp);
+
+            if (tempDistance < bestDistance) {
+                best = temp;
+            }
+
+            //clearPreviousGeneration();
 		}
 
-        temp = findShortest(multiTours);
-		
-		int[] shortest = new int[temp.size()];
+		int[] shortest = new int[best.size()];
 
 		int j = 0;
-        for (Node aTemp : temp) {
-            shortest[j] = aTemp.getID();
+        for (Node node : best) {
+            shortest[j] = node.getID();
             j++;
         }
 		
-		Distance_2 = totalDistance(temp);
+		//Distance_2 = totalDistance(temp);
 
 		return shortest;
 	}
 
+    private void clearPreviousGeneration() {
+        nextGeneration.clear();
+    }
 
-    private ArrayList<ArrayList<Node>> EA(ArrayList<ArrayList<Node>> pop, double cross_rate, double mut_rate) {
 
-		ArrayList<ArrayList<Node>> nextGeneration = new ArrayList<ArrayList<Node>>(pop.size());
+    private ArrayList<ArrayList<Node>> GA(ArrayList<ArrayList<Node>> pop, double cross_rate, double mut_rate) {
+
+        nextGeneration = new ArrayList<ArrayList<Node>>();
 		ArrayList<Node> parent1;
 		ArrayList<Node> parent2;
+
+        int popSize = pop.size();
 		
-		for(int j = 0; j < pop.size(); j++) {
+		for(int j = 0; j < popSize; j++) {
 			parent1 = Selection_Tournament(5, pop);
 			parent2 = Selection_Tournament(5, pop);
 			
 			if(Math.random() <= cross_rate){
-                HashMap<Integer, Node> lookup_table = new HashMap<Integer, Node>(singleTour.size());
-
-                for (Node node: singleTour)
-                    lookup_table.put(node.getID(), node);
 				
-				Crossover_Edge_Recombination(parent1, parent2, lookup_table);
-				Crossover_Edge_Recombination(parent2, parent1, lookup_table);
+				Crossover_Crossover_PMX(parent1, parent2);
+				Crossover_Crossover_PMX(parent2, parent1);
 
                 nextGeneration.add(offsprings.get(0));
                 nextGeneration.add(offsprings.get(1));
@@ -84,7 +96,7 @@ public class TSP {
 			offsprings.clear();		
 		}
 		
-		for(int j = 0; j < pop.size(); j++){
+		for(int j = 0; j < popSize; j++){
 			
 			if(Math.random() <= mut_rate) {
 				
@@ -95,9 +107,123 @@ public class TSP {
 			
 		return nextGeneration;
 	}
-	
-	
-	private void generatePopulation(int popSize, boolean ifInitial){
+
+    private void Crossover_Crossover_PMX(ArrayList<Node> tour_1, ArrayList<Node> tour_2) {
+
+        int size = tour_1.size();
+
+        Random rand = new Random();
+        int number1, number2;
+        //Choose two arbitrary parts for start and end from parents
+        do {
+            number1 = rand.nextInt(size);
+            number2 = rand.nextInt(size);
+        } while (number1 == number2);
+
+        int start = Math.min(number1, number2);
+        int end = Math.max(number1, number2);
+        //System.out.println("start: " + start + ", end: " + end);
+
+        //Initialise children
+        List<Node> child_1 = Arrays.asList(new Node[size]);
+        List<Node> child_2 = Arrays.asList(new Node[size]);
+
+        //Copy the section from the parents to the children
+        for (int i = start; i <= end; i++) {
+            child_1.set(i, tour_1.get(i));
+            child_2.set(i, tour_2.get(i));
+        }
+
+        //Starting from start point, look for elements on that section of the Parent2 that have not been copied.
+        //For each of these (say i) look in the Child1 to see what element (say j) has been copied in its place from Parent1.
+        //Place i in the position occupied by j in Parent2.
+        Node replaceNode_1, replaceNode_2;
+
+        for (int i = start; i <= end; i++) {
+            replaceNode_1 = tour_1.get(i);
+            replaceNode_2 = tour_2.get(i);
+
+            //If the node has already been copied to the child, take no action.
+            //Otherwise get the gene that is on the same index on the other parent.
+            //Find the position that that gene occupies on this parent.
+
+            if (!child_1.contains(replaceNode_2)) {
+                int indexP2j = tour_2.indexOf(replaceNode_1);
+
+                Node nodeP2j= child_1.get(indexP2j);
+
+                if (nodeP2j == null){
+                    child_1.set(indexP2j, replaceNode_2);
+                } else {
+                    //If the place occupied by j in Parent2 has already been filled
+                    //in child1, by an element k, put i in the position occupied by k in Parent2.
+                    int indexP2k = tour_2.indexOf(nodeP2j);
+
+                    //Verify that the child has no node on this position.
+                    if (child_1.get(indexP2k) == null){
+                        child_1.set(indexP2k, replaceNode_2);
+                    }
+                }
+            }
+
+            if (!child_2.contains(replaceNode_1)) {
+                int indexP1j = tour_1.indexOf(replaceNode_2);
+
+                Node nodeP1j= child_2.get(indexP1j);
+
+                if (nodeP1j == null){
+                    child_2.set(indexP1j, replaceNode_1);
+                } else {
+                    int indexP1k = tour_1.indexOf(nodeP1j);
+
+                    if (child_2.get(indexP1k) == null){
+                        child_2.set(indexP1k, replaceNode_1);
+                    }
+                }
+            }
+        }
+
+        //The remaining elements are placed by verifying which nodes are in Parent2 that don't exist in Child1 and copy them.
+        ArrayList<Node> copy_tour_1 = new ArrayList<Node>(tour_1);
+        ArrayList<Node> copy_tour_2 = new ArrayList<Node>(tour_2);
+
+        ArrayList<Integer> emptyList_1 = new ArrayList<Integer>();
+        ArrayList<Integer> emptyList_2 = new ArrayList<Integer>();
+
+        //For each element in Child1, if it is null, put its index in emptyList1, otherwise remove the element from Parent2.
+        for (int i = 0; i < size; i++) {
+            Node temp_node1 = child_1.get(i);
+            Node temp_node2 = child_2.get(i);
+
+            if (temp_node1 == null) {
+                emptyList_1.add(i);
+            } else {
+                copy_tour_2.remove(temp_node1);
+            }
+
+            if (temp_node2 == null) {
+                emptyList_2.add(i);
+            } else {
+                copy_tour_1.remove(temp_node2);
+            }
+        }
+
+        //Put rest nodes from Parent2 in the empty places
+        for (int i: emptyList_1) {
+            child_1.set(i, copy_tour_2.remove(0));
+        }
+
+        for (int i: emptyList_2) {
+            child_2.set(i, copy_tour_1.remove(0));
+        }
+
+        //Add to the offspring list
+        offsprings.add(new ArrayList<Node>(child_1));
+        offsprings.add(new ArrayList<Node>(child_2));
+    }
+
+
+    private void generatePopulation(int popSize, boolean ifInitial){
 		
 		multiTours = new ArrayList<ArrayList<Node>>(popSize);
 		
@@ -148,206 +274,7 @@ public class TSP {
 
 	}
 
-    // Edge Recombination Crossover
-    public void Crossover_Edge_Recombination(ArrayList<Node> cities_1, ArrayList<Node> cities_2, HashMap<Integer, Node> lookup_table) {
 
-        int size = cities_1.size();
-
-        //Create the edge map
-        HashMap<Integer, HashSet<Integer>> edgeMap = new HashMap<Integer, HashSet<Integer>>();
-
-        for (int index_1 = 0; index_1 < cities_1.size(); index_1++) {
-
-            int city_1_id = cities_1.get(index_1).getID();
-
-            int index_2 = 0;
-            //Get corresponding location in parent 2
-            for (int i = 0; i < cities_2.size(); i++) {
-
-                if (cities_2.get(i).getID() == city_1_id) {
-                    index_2 = i;
-                    break;
-                }
-            }
-
-            edgeMap.put(city_1_id, getEdges(cities_1, cities_2, index_1, index_2));
-        }
-
-        ArrayList<Node> childCity = new ArrayList<Node>(size);
-        ArrayList<Integer> unvisited_cityIDs = new ArrayList<Integer>(size);
-        for (int i = 1; i <= size; ++i) {
-            unvisited_cityIDs.add(i);
-        }
-
-        //Pick and store the first city
-        Node current_city;
-        int parent1_connection_degrees = edgeMap.get(cities_1.get(0).getID()).size();
-        int parent2_connection_degrees = edgeMap.get(cities_2.get(0).getID()).size();
-
-        if (parent1_connection_degrees >= parent2_connection_degrees) {
-            current_city = cities_1.get(0);
-        } else {
-            current_city = cities_2.get(0);
-        }
-        childCity.add(current_city);
-
-        //Remove corresponding entries
-        unvisited_cityIDs.remove(unvisited_cityIDs.indexOf(current_city.getID()));
-        edgeMap = removeFromEdgeMap(edgeMap, current_city.getID());
-
-
-        while (!unvisited_cityIDs.isEmpty()) {
-
-            if (!edgeMap.get(current_city.getID()).isEmpty()) {
-                current_city = pickNextCity(lookup_table, edgeMap, current_city.getID());
-
-            } else {
-
-                int nextCityId;
-                if (unvisited_cityIDs.size() == 1) {
-                    nextCityId = unvisited_cityIDs.get(0);
-                }
-                else {
-                    Random rand = new Random();
-                    nextCityId = unvisited_cityIDs.get(rand.nextInt(unvisited_cityIDs.size() - 1));
-                }
-
-                current_city = lookup_table.get(nextCityId);
-            }
-
-            childCity.add(current_city);
-
-            unvisited_cityIDs.remove(unvisited_cityIDs.indexOf(current_city.getID()));
-            edgeMap = removeFromEdgeMap(edgeMap, current_city.getID());
-        }
-
-        offsprings.add(childCity);
-    }
-
-
-    private HashSet<Integer> getEdges(ArrayList<Node> cities_1, ArrayList<Node> cities_2, int index_1, int index_2) {
-
-        HashSet<Integer> edges = new HashSet<Integer>();
-        int size = cities_1.size();
-
-        //Get parent1's edges
-        int front = (index_1 + 1) % size;
-        int back = (size + index_1 - 1) % size;
-        edges.add(cities_1.get(front).getID());
-        edges.add(cities_1.get(back).getID());
-
-        //Get parent2's edges
-        front = (index_2 + 1) % size;
-        back = (size + index_2 - 1) % size;
-
-        //Check if the city is already in parent1's list, if so, negate it
-        int[] frontback = {front, back};
-        for (int i = 0; i < 2; i++) {
-
-            int id = cities_2.get(frontback[i]).getID();
-            if (edges.contains(id)) {
-                edges.remove(id);
-                edges.add(-1 * id);
-            } else
-                edges.add(id);
-        }
-
-        return edges;
-    }
-
-
-    private HashMap<Integer, HashSet<Integer>> removeFromEdgeMap(HashMap<Integer, HashSet<Integer>> edgeMap, int id) {
-
-        //Remove the given ID from all entries in edge map
-        for (Map.Entry<Integer, HashSet<Integer>> edge : edgeMap.entrySet()) {
-
-            HashSet<Integer> connections = edge.getValue();
-            connections.remove(id);
-            connections.remove(-1 * id);
-        }
-
-        return edgeMap;
-    }
-
-
-    private Node pickNextCity(HashMap<Integer, Node> lookup_table, HashMap<Integer, HashSet<Integer>> edgeMap, int id) {
-
-        Node next_city;
-        ArrayList<Integer> possible_cities = new ArrayList<Integer>(edgeMap.get(id));
-
-        // 3 Possibilities:
-        // 4 cities to consider: all positive.
-        // 3 cities to consider: one of them could be negative.
-        // 2 cities to consider: both could be negative.
-
-        if (possible_cities.size() == 3) {
-            //Pick the negative one if it exists.
-            for (int i = 0; i < 3; i++) {
-
-                if (possible_cities.get(i) < 0) {
-                    next_city = lookup_table.get(-1 * possible_cities.get(i));
-                    return next_city;
-                }
-            }
-
-        } else if (possible_cities.size() == 1) {
-            next_city = lookup_table.get(Math.abs(possible_cities.get(0)));
-            return next_city;
-        }
-
-        //If 1 of the 2 is negative, pick it for as the next city
-        if (possible_cities.size() == 2) {
-
-            int negs = 0;
-            int index = 0;
-
-            if (possible_cities.get(0) < 0) {
-                ++negs;
-                index = 0;
-            }
-
-            if (possible_cities.get(1) < 0) {
-                ++negs;
-                index = 1;
-            }
-
-            if (negs == 1) {
-                next_city = lookup_table.get(Math.abs(possible_cities.get(index)));
-                return next_city;
-            }
-        }
-
-        //If not picking a negative, or if all are negative, pick the one with the least connections.
-        int min_connections = Integer.MAX_VALUE;
-        ArrayList<Integer> possible_choices = new ArrayList<Integer>();
-
-        for (Map.Entry<Integer, HashSet<Integer>> edge: edgeMap.entrySet()) {
-            // City in edge map listing could be positive or negative.
-            if (possible_cities.contains(edge.getKey()) || possible_cities.contains(-1 * edge.getKey())) {
-
-                int num_connections = edge.getValue().size();
-                if (num_connections < min_connections) {
-
-                    min_connections = num_connections;
-                    possible_choices.clear();
-                    possible_choices.add(edge.getKey());
-
-                } else if (edge.getValue().size() == min_connections) {
-                    possible_choices.add(edge.getKey());
-                }
-            }
-        }
-
-        //If tie for the least connections, randomly choose.
-        if (possible_choices.size() == 1) {
-            next_city = lookup_table.get(possible_choices.get(0));
-        } else {
-            Random rand = new Random();
-            next_city = lookup_table.get(Math.abs(possible_choices.get(rand.nextInt(possible_choices.size() - 1))));
-        }
-
-        return next_city;
-    }
 	
 	
 	private ArrayList<Node> findShortest(ArrayList<ArrayList<Node>> tours){
