d99e74badfbb8b6b58d5b8242f2dcea62332f73c
zyksnowking@gmail.com <zyksnowking@gmail.com@4d6728cc-4268-c6eb-ead0-70304b354550>
2014-10-05 09:00:48 +0000

    Daniel (CrossOverTTP final 1)

---
+++ exercise4/src/ttp/Node.java
@@ -0,0 +1,22 @@
+package ttp;
+
+/**
+ *
+ * @author Zhu Zheng
+ */
+
+public class Node {
+    //The id of the node.
+    private final int id;
+
+    //Node constructor
+    public Node(int id) {
+        super();
+        this.id = id;
+    }
+
+    //Return the id of the node
+    public int getID() {
+        return id;
+    }
+}
