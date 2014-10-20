33816aad4c017e23b01d44e144b11736bd2240b2
mrcotter2013@gmail.com <mrcotter2013@gmail.com@4d6728cc-4268-c6eb-ead0-70304b354550>
2014-10-20 14:56:20 +0000

    evaluate funtcion updated, not done yet. Other minor changes

--- trunk/MultiOB/exercise2-4/src/problems/TTP.java
+++ trunk/MultiOB/exercise2-4/src/problems/TTP.java
@@ -15,19 +15,19 @@ import java.io.*;
 
 public class TTP extends Problem {
 
-    public String      name;
-    public String      knapsackDataType;
+    //public String      name;
+    //public String      knapsackDataType;
     public int         numberOfNodes;
     public int         numberOfItems;
     public long        capacityOfKnapsack;
     public double      minSpeed;
     public double      maxSpeed;
     public double      rentingRatio;
-    public String      edgeWeightType;
+    //public String      edgeWeightType;
     public double[][]  nodes;
     public int[][]     items;
 
-    public double [][] distanceMatrix_ ;
+    public long [][] distanceMatrix_;
 
     /**
      * Creates a new TTP problem instance. It accepts data files from a ttp file
@@ -38,6 +38,14 @@ public class TTP extends Problem {
         numberOfObjectives_ = 2;
         numberOfConstraints_= 0;
         problemName_        = "TTP";
+
+        upperLimit_ = new double[numberOfVariables_];
+        lowerLimit_ = new double[numberOfVariables_];
+
+        for (int var = 0; var < numberOfVariables_; var++){
+            lowerLimit_[var] = 0.0;
+            upperLimit_[var] = 1.0;
+        }
 
         length_ = new int[numberOfVariables_];
 
@@ -63,12 +71,44 @@ public class TTP extends Problem {
     @Override
     public void evaluate(Solution solution) throws JMException {
 
+        // Correctness check: does the tour start and end in the same city
+        int firstCity = ((Permutation) solution.getDecisionVariables()[0]).vector_[0];
+        int lastCity  = ((Permutation) solution.getDecisionVariables()[0]).vector_[numberOfNodes - 1];
+
+        if(firstCity != lastCity) {
+            System.err.println("ERROR: The last city must be the same as the first city");
+            System.exit(1);
+        }
+
+        double fitness1_td = 0.0;
+        double fitness2_ob = 0.0;
+
+        // Calculate fitness 1 - total distance
+        for (int i = 0; i < numberOfNodes - 1; i++) {
+
+            int x = ((Permutation) solution.getDecisionVariables()[0]).vector_[i];
+            int y = ((Permutation) solution.getDecisionVariables()[0]).vector_[i+1];
+
+            fitness1_td += distanceMatrix_[x][y];
+        }
+        fitness1_td += distanceMatrix_[firstCity][lastCity];
+
+
+        // Calculate fitness 2 - objective value of a given tour
+        for (int i = 0; i < numberOfNodes - 1; i++) {
+
+            int currentCityTEMP = ((Permutation)solution.getDecisionVariables()[0]).vector_[i];
+            // what's the current city? --> but the items start at city 2 in the TTP file, so I have to take another 1 off!
+            int currentCity = currentCityTEMP-1;
+
+        }
+
     }
 
 
-    private double[][] readProblem(String fileName) throws IOException {
+    private long[][] readProblem(String fileName) throws IOException {
 
-        double[][] matrix = null;
+        long[][] matrix;
         BufferedReader br = new BufferedReader(new InputStreamReader(
                 new FileInputStream(fileName)));
 
@@ -77,7 +117,7 @@ public class TTP extends Problem {
             while ((line = br.readLine()) != null) {
                 // process the line
 
-                if (line.startsWith("PROBLEM NAME")) {
+                /*if (line.startsWith("PROBLEM NAME")) {
                     line = line.substring(line.indexOf(":")+1);
                     line = line.replaceAll("\\s+","");
                     this.name = line;
@@ -86,7 +126,7 @@ public class TTP extends Problem {
                     line = line.substring(line.indexOf(":")+1);
                     line = line.replaceAll("\\s+","");
                     this.knapsackDataType = line;
-                }
+                }*/
                 if (line.startsWith("DIMENSION")) {
                     line = line.substring(line.indexOf(":")+1);
                     line = line.replaceAll("\\s+","");
@@ -117,11 +157,11 @@ public class TTP extends Problem {
                     line = line.replaceAll("\\s+","");
                     this.rentingRatio=Double.parseDouble(line);
                 }
-                if (line.startsWith("EDGE_WEIGHT_TYPE")) {
+                /*if (line.startsWith("EDGE_WEIGHT_TYPE")) {
                     line = line.substring(line.indexOf(":")+1);
                     line = line.replaceAll("\\s+","");
                     this.edgeWeightType = line;
-                }
+                }*/
                 if (line.startsWith("NODE_COORD_SECTION")) {
                     this.nodes = new double[this.numberOfNodes][3];
                     for (int i=0; i<this.numberOfNodes; i++) {
@@ -158,10 +198,10 @@ public class TTP extends Problem {
 
         double dist;
         long distance;
-        matrix = new double[numberOfNodes][numberOfNodes];
+        matrix = new long[numberOfNodes][numberOfNodes];
 
         for (int i = 0; i < numberOfNodes; i++) {
-            matrix[i][i] = 0.0;
+            matrix[i][i] = 0;
             for (int j = i + 1; j < numberOfNodes; j++) {
                 dist = Math.sqrt(
                                 (this.nodes[i][1]-this.nodes[j][1]) *
