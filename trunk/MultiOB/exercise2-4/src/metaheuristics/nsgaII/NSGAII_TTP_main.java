f979ed60c719a5bc4253465cf356dc2ea9063665
mrcotter2013@gmail.com <mrcotter2013@gmail.com@4d6728cc-4268-c6eb-ead0-70304b354550>
2014-10-22 07:47:33 +0000

    Combine inversion mutation and bit flip mutation into one mutation operator for TTP Other updates

--- trunk/MultiOB/exercise2-4/src/metaheuristics/nsgaII/NSGAII_TTP_main.java
+++ trunk/MultiOB/exercise2-4/src/metaheuristics/nsgaII/NSGAII_TTP_main.java
@@ -7,7 +7,7 @@ import jmetal.core.SolutionSet;
 import jmetal.operators.crossover.CrossoverFactory;
 import jmetal.operators.mutation.MutationFactory;
 import jmetal.operators.selection.SelectionFactory;
-import jmetal.problems.mTSP;
+import jmetal.problems.TTP;
 import jmetal.qualityIndicator.QualityIndicator;
 import jmetal.util.Configuration;
 import jmetal.util.JMException;
@@ -34,6 +34,7 @@ public class NSGAII_TTP_main {
      * @throws ClassNotFoundException
      * Usage:
      *      - jmetal.metaheuristics.nsgaII.NSGAII_TTP_main
+     *      - jmetal.metaheuristics.nsgaII.NSGAII_TTP_main ttp_fileName
      */
 
     public static void main(String [] args) throws
@@ -41,6 +42,45 @@ public class NSGAII_TTP_main {
                                     SecurityException,
                                     IOException,
                                     ClassNotFoundException {
+        Problem   problem = null;   // The problem to solve
+        Algorithm algorithm ;       // The algorithm to use
+        Operator  crossover ;       // Crossover operator
+        Operator  mutation  ;       // Mutation operator
+        Operator  selection ;       // Selection operator
+
+        HashMap  parameters ; // Operator parameters
+
+        QualityIndicator indicators ; // Object to get quality indicators
+
+        // Logger object and file to store log messages
+        logger_      = Configuration.logger_ ;
+        fileHandler_ = new FileHandler("NSGAII_main.log");
+        logger_.addHandler(fileHandler_) ;
+
+        indicators = null ;
+
+        if (args.length == 0) {
+            problem = new TTP("PermutationArrayInt", "a280_n279_bounded-strongly-corr_01.ttp");
+        }
+        else if (args.length == 1) {
+            problem = new TTP("PermutationArrayInt", args[0]);
+        } else {
+            System.err.println("Usage: - jmetal.metaheuristics.nsgaII.NSGAII_TTP_main ttp_fileName");
+            System.exit(1);
+        }
+
+        algorithm = new NSGAII(problem);
+
+        // Algorithm parameters
+        algorithm.setInputParameter("populationSize",10);
+        algorithm.setInputParameter("maxEvaluations",100000);
+
+        /* Crossver operator */
+        parameters = new HashMap() ;
+        parameters.put("probability", 0.95) ;
+        crossover = CrossoverFactory.getCrossoverOperator("PMXCrossover_TTP", parameters);
+
+
 
     }
 }
