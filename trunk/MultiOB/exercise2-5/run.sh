a177b1c7075fe9aa144297b0506dfc797ea7c2ab
mrcotter2013@gmail.com <mrcotter2013@gmail.com@4d6728cc-4268-c6eb-ead0-70304b354550>
2014-10-23 07:25:43 +0000

    Minor updates to ant build, script

--- trunk/MultiOB/exercise2-5/run.sh
+++ trunk/MultiOB/exercise2-5/run.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
 #
-# Run script by adding a parameter indicating metaheuristic algorithms.
+# Run script by adding a argument indicating metaheuristic algorithms.
 # e.g., ./run.sh nsgaii      ./run.sh spea2        ./run.sh ibea
-# 
+# If no argument supplied, NSGA-II will be used as deafult algorithm.
+#
 # The porgram can receive 1 argument that indicates the ttp file name
 # arg0  ttp filename
 #
 
+if [ $# -eq 0 ]
+  then
+    echo "No algorithm supplied"
+    echo "NSGA-II will be used as default algorithm"
 
-ant $1 -Darg0="a280_n279_bounded-strongly-corr_01.ttp"
+    #ant nsgaii -Darg0="a280_n279_bounded-strongly-corr_01.ttp"
+	#ant nsgaii -Darg0="a280_n1395_uncorr-similar-weights_05.ttp"
+	#ant nsgaii -Darg0="a280_n2790_uncorr_10.ttp"
+	ant nsgaii -Darg0="fnl4461_n4460_bounded-strongly-corr_01.ttp"
+	#ant nsgaii -Darg0="pla33810_n33809_bounded-strongly-corr_01.ttp"
 
-#ant $1 -Darg0="a280_n1395_uncorr-similar-weights_05.ttp"
+  else
+  	ant $1 -Darg0="a280_n279_bounded-strongly-corr_01.ttp"
+	#ant $1 -Darg0="a280_n1395_uncorr-similar-weights_05.ttp"
+	#ant $1 -Darg0="a280_n2790_uncorr_10.ttp"
+	#ant $1 -Darg0="fnl4461_n4460_bounded-strongly-corr_01.ttp"
+	#ant $1 -Darg0="pla33810_n33809_bounded-strongly-corr_01.ttp"
+fi
 
-#ant $1 -Darg0="a280_n2790_uncorr_10.ttp"
-
-#ant $1 -Darg0="fnl4461_n4460_bounded-strongly-corr_01.ttp"
-
-#ant $1 -Darg0="pla33810_n33809_bounded-strongly-corr_01.ttp"
